7fd0e32a195749e4f02c8e8e61631128c704cd12
ChanMor <morelos.franzchristian@gmail.com>
2024-03-29 23:03:25 +0800

    Created disaster classifier model

---
+++ dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+conflict = pd.read_csv('preprocessed_data/conflict.csv')
+
+earthquake = pd.read_csv('preprocessed_data/earthquake.csv')
+
+fire = pd.read_csv('preprocessed_data/fire.csv')
+
+typhoon = pd.read_csv('preprocessed_data/typhoon.csv')
+
+volcanic = pd.read_csv('preprocessed_data/volcanic.csv')
